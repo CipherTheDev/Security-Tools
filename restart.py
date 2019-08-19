cf93b548aef3d1f5394ca62c5b575f8d5628c617
CipherTheDev <19726555+pastaboy2131@users.noreply.github.com>
2019-08-19 21:09:39 +1000

    Update restart.py

--- restart.py
+++ restart.py
@@ -5,5 +5,12 @@ import socket
 
 class restart_app():
     print("""If you are seeing this, this is cause of the main software failed to respond and this will take over. """)
+    
+    def __init__():
+        self.script =  None
+        self.IP = None
+        self.PORT  = None
+        
+    
 
     
