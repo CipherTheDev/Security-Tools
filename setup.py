d85169ada95ccaecd20381046aaaea179319496f
CipherTheDev <19726555+pastaboy2131@users.noreply.github.com>
2020-12-19 18:22:20 +1100

    Update release 0.1.3
    This update allows the API to print out installation issues and/or related problems, yet to be updated with more code to achieve this.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import *
 import time
+import requests
 """
 Below is the setup that may require some API's and/or wrappers.
 ##############################################################
@@ -10,9 +11,27 @@ try:
     time.sleep(20)
     f = open("Output.html" , "w")
     f.write("<h1>Setup completed</h1>")
-    f.writelines("<kbd>Once completed, please head over the CLI.</kbd>\nOtherwise find the relavent information below")
+    f.writelines("Once completed, please head over the CLI.\nOtherwise find the relavent information below")
     f.close()
-
+    print("Setup has been completed. Refer towards the manual")
 
 except Exception as e:
     print("An error has caused the setup to hault. Please check below." + e)
+    print('[+] Installation has been repeated')
+
+    f.open("reinstall.htm" , "w+a")
+    with f:
+        f.writelines("<style>p{ color: green;} body{background-image: https://img1.goodfon.com/original/1920x1080/0/d2/cvety-zelen-trava-lug-pole.jpg?d=1}")
+
+class Gather_Report():
+    pass
+
+class Print_report(Gather_Report):
+    pass
+
+
+
+Get_RPT = Gather_Report()
+if __name__  == "__main__":
+
+Get_RPT()
