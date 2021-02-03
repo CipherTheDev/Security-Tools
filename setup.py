078e8ec7a18b6f61c57831db667f2b6d78937e2e
CipherTheDev <19726555+pastaboy2131@users.noreply.github.com>
2021-02-04 01:27:21 +1100

    Options add for user interface

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import *
 import time
 import requests
 import argparse
+import sys , os
 """
 Below is the setup that may require some API's and/or wrappers.
 ##############################################################
@@ -9,7 +10,7 @@ Below is the setup that may require some API's and/or wrappers.
 
 try:
     print("Starting setup.py ... please wait")
-    time.sleep(20)
+    #time.sleep(20)
     f = open("Output.html" , "w")
     f.write("<h1>Setup completed</h1>")
     f.writelines("Once completed, please head over the CLI.\nOtherwise find the relavent information below")
@@ -25,18 +26,37 @@ except Exception as e:
         f.writelines("<style>p{ color: green;} body{background-image: https://img1.goodfon.com/original/1920x1080/0/d2/cvety-zelen-trava-lug-pole.jpg?d=1}")
 
 class Gather_Report():
-    print("use the following arguments in order to open the report")
-    print("-h html , -t text , -w .docx and/or default by web layout")
+    def __init__(self):
+        self.formname = None
+        self.arguments = None 
+   
     pass
 
+    def Show_Output(self):
+        print("use the following arguments in order to open the report")
+        print("-h html , -t text , -w .docx and/or default by web layout")
+        parser = argparse.ArgumentParser(description="Opens within HTML format" , usage="" , prog="")
+        parser.add_argument('-ho' , '--html' ,  type=str , metavar='', required=True , help='Open\'s within HTML')
+        parser.add_argument('-t' ,'--text' , type=str , metavar='',required=True, help='Open\'s within a text file')
+        parser.add_argument('-w' , '--web' , type=str , metavar='', required=True , help='Open\'s within a default web layout. REQUIRES NETWORK CONNECTION')
+        args = parser.parse_args()
+        try:
+            print(args.html)
+            print(args.text)
+            print(args.web)
+        except:
+            print("Wrong value and/or application crashed.")
+        pass
 class Print_report(Gather_Report):
     print(""""
     use the following arguemnts to print the report either in a browser and/or text editor
     
     -o ~/root/example.html , -ho PRINTS THE HTML DOCUMENT,   -d PRINTS DEFAULT FORMAT IN WEB LAYOUT
     """)
+    
     pass
 
 
 
 Get_RPT = Gather_Report()
+Get_RPT.Show_Output()
