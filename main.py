774584079c6ce30234a46789538f65793675d1bb
CipherTheDev <19726555+pastaboy2131@users.noreply.github.com>
2021-03-05 17:50:53 +1100

    Added functionalities

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Command():
         print("Please wait a minute for the Buffer Overflow menu to display")
         time.sleep(10000)
         print("Buffer Overflow menu: -s STACKOVERFLOW , -H HEAP OVERFLOW  , -D DEFAULT ATTACK METHOD, HAIL MARY")
-        pass
+        
 
     def Denial_Service(self):
         pass
@@ -32,17 +32,31 @@ class Command():
         print("Usage: D3sTroYer.py https://www.example.com/  -g  -o ~/root/Desktop/example.html")
         pass
 
+class Interface(Command):
 
+    def __init__(self):
+        pass
+        
+    def port_scn(self) -> str:
+        self.prsr = None
+        prsr = argparse.ArgumentParser()
+        self.arg = prsr
+        prsr.add_argument('--ip' , metavar='IPv4/IPv6' , type=str , help="IPv4/v6 of victim")
+        prsr.add_argument('-d' , metavar='Denial Of Sevice or Distributed Denial Of Service' , type=str , help="Usage: Denial Of Service")
+        prsr.add_argument('-nmp' , metavar='Network Mapping' , type=int , help="Usage: Provides Network Mapping functionalities")
+        prsr.add_argument('-g' , metavar='Usage: ' , type=str , help="Usage: ")
+        prsr.print_help()
+        self.arg = prsr.parse_args()
+        pass
+
+test = Interface()
+test.port_scn()
 """"
 the following is the documentation for this command and control script,
 please note that if any modifications under the licence of GPLv3, therefore the original author must be mentioned
 Author details,
-
 Name: Parsa Zarrin
-
 Social Media: hackforums.com/TheHiddenCoder, instagram.com/parsathedev
-
 Location: Australia
-
 Purpose of script: To aid 
 """
